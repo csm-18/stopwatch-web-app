7ccb23b0b85617f5374856959410bfd420d47dde
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-05 12:38:57 +0530

    vue setup and add Heading(Timer)

---
+++ frontend/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: { title: 'Timer' }, // Add title here
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  const defaultTitle = 'Timer' // Fallback title
+  document.title = String(to.meta.title) || defaultTitle
+  next() // Don't forget to call next() to resolve the navigation
+})
+
+export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="main-con">
+    <h1>Timer</h1>
+  </div>
+</template>
+<style scoped lang="scss">
+.main-con {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    text-align: center;
+    font-family: 'Baloo 2', sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 800;
+    font-style: normal;
+    font-size: 3rem;
+    background: #30cfb7;
+    border-radius: 50px;
+    padding: 0.8rem 5rem;
+    margin: 1rem;
+  }
+}
+</style>
